6504a0641193d0309285b60794505d7496da64fe
mugensnaps <rayyantaib@outlook.com>
2025-03-11 10:54:36 +0000

    Add files via upload

--- script.js
+++ script.js
@@ -1,23 +1,16 @@
-// Function to show the clicked tab and hide the others
-function openTab(evt, tabName) {
-    var i, tabcontent, tablinks;
+document.addEventListener(DOMContentLoaded, function() {
+    const images = document.querySelectorAll(.grid img);
 
-    // Hide all tab content
-    tabcontent = document.getElementsByClassName("tabcontent");
-    for (i = 0; i < tabcontent.length; i++) {
-        tabcontent[i].style.display = "none";
-    }
+    images.forEach(img = {
+        img.addEventListener(click, () = {
+            const modal = document.createElement(div);
+            modal.classList.add(modal);
+            modal.innerHTML = `img src=${img.src} class=modal-image`;
+            document.body.appendChild(modal);
 
-    // Remove "active" class from all tab links
-    tablinks = document.getElementsByClassName("tablink");
-    for (i = 0; i < tablinks.length; i++) {
-        tablinks[i].classList.remove("active");
-    }
-
-    // Show the clicked tab content and add "active" class to the tab link
-    document.getElementById(tabName).style.display = "block";
-    evt.currentTarget.classList.add("active");
-}
-
-// Set default tab to show (Portfolio in this case)
-document.getElementsByClassName("tablink")[0].click();
+            modal.addEventListener(click, () = {
+                modal.remove();
+            });
+        });
+    });
+});
