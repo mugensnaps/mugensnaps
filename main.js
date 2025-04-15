1eb79ad75e348935b97973ef7821febab4ad0f68
mugensnaps <rayyantaib@outlook.com>
2025-04-15 13:08:37 +0100

    Add files via upload

--- main.js
+++ main.js
@@ -14,3 +14,34 @@ navLinks.addEventListener("click", (e) => {
     menuBtnIcon.setAttribute("class", "ri-menu-line")
 });
 
+const scrollRevealOption = {
+    distance:"50px",
+    origin:"bottom",
+    duration: 1000,
+};
+
+ScrollReveal().reveal(".about_container .section_header", {
+    ...scrollRevealOption,
+});
+ScrollReveal().reveal(".about_container .section_description", {
+    ...scrollRevealOption,
+    delay: 500,
+    interval: 500,
+});
+ScrollReveal().reveal(".about_container img", {
+    ...scrollRevealOption,
+    delay: 1000,
+});
+
+ScrollReveal().reveal(".service_container .section_header", {
+    ...scrollRevealOption,
+});
+ScrollReveal().reveal(".service_container .section_description", {
+    ...scrollRevealOption,
+    delay: 500,
+});
+ScrollReveal().reveal(".service_card", {
+    duration: 1000,
+    delay: 1000,
+    interval: 500,
+});
